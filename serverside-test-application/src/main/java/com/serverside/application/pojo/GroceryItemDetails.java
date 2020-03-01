bf0e857d4df17ff9ccf85a85fc86a5198ef8f89f
yassine.mandaris <yassine.mandaris@aeroparker.com>
2020-03-01 21:07:14 +0000

    Create GroceryItemDetails class
    -GroceryItemDetails is a pojo that has fields title, kcal_per_100g,
    description and unit_price. GroceryItemDetails is populated every time
    we fetch the information related to an item. so an instance is created
    for each product, populated and then mapped as json object.

---
+++ serverside-test-application/src/main/java/com/serverside/application/pojo/GroceryItemDetails.java
@@ -0,0 +1,18 @@
+package com.serverside.application.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author yassine.mandaris
+ *
+ */
+@Getter
+@Setter
+public class GroceryItemDetails
+{
+	private String title;
+	private String kcal;
+	private String unitPrice;
+	private String description;
+}
